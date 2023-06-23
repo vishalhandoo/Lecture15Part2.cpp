62e71b22e63b1ba31be8d896537bea8d83ecb668
vishalhandoo <81742803+vishalhandoo@users.noreply.github.com>
2023-06-23 20:25:56 +0530

    Add files via upload

---
+++ Lecture15Part2.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    string a;
+    string b;
+
+    string c;
+    
+    getline(cin,c);
+    
+
+    cout<<"The concatinated stringd are : "<<c<<endl;
+    
+    return 0;
+}
